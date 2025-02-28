1cc91d51db345d20f011a196f1c22eee35010413
Guilherme Dias Machado <pagani.dias2@gmail.com>
2025-02-28 12:52:11 -0300

    IFs

---
+++ Declaração IF/EXBase.js
@@ -0,0 +1,12 @@
+let num = 100
+if(num > 10){
+    console.log("Numeral maior que 10")
+   if(num >= 50){
+    console.log("Numeral maior ou igual à 50")
+   }
+}else if(num > 5){
+    console.log("Numeral está entre 6 e 10")
+}else{
+    console.log("Numeral menor ou igual à 5")
+}
+console.log("Fim")
---
+++ Declaração IF/EXBase_02.js
@@ -0,0 +1,8 @@
+let energia = 100
+let clima="chovendo"
+if(energia > 70 && clima=="sol"){
+    console.log("Vou a praia")
+}else{
+    console.log("Vou ao cinema")
+}
+console.log("Fim")
