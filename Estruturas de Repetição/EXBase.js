cee6087fb97a5bf17970ed261be103699d06cd3c
Guilherme Dias Machado <pagani.dias2@gmail.com>
2025-03-03 13:48:19 -0300

    For IN e For OF

---
+++ Estruturas de Repetição/EXBase.js
@@ -0,0 +1,12 @@
+console.log("Inicio")
+
+for(let i=0; i < 100; i++){
+    
+    if(i%2){
+        console.log(i+ " é par")
+    }else{
+        console.log(i+ " é impar")   
+    }
+}
+
+console.log("Fim")
---
+++ Estruturas de Repetição/ForIn ForOf.js
@@ -0,0 +1,10 @@
+let num = [10, 20, 30, 40, 50]
+for(n of num){
+    console.log(n)
+}
+//for (n in num){
+   // console.log(num[n])
+// só funciona com N indicador #2} 
+//#3 for(let i=0; i<num.length; i++){
+    //console.log(num[i])//add num[]para imprimir o array
+//}
