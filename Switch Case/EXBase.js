99131c6ba02a7a7abbdb666fec837ad9354dba05
Guilherme Dias Machado <pagani.dias2@gmail.com>
2025-03-03 12:31:27 -0300

    Switch Case

---
+++ Switch Case/EXBase.js
@@ -0,0 +1,20 @@
+let ranking = 7
+switch(ranking){
+    case 1:
+        console.log("Primeiro Lugar")
+        break
+
+    case 2:
+        console.log("Segundo Lugar")
+        break 
+    
+    case 3:
+        console.log("Terceiro Lugar")
+        break
+
+    case 4: case 5: case 6:
+        console.log("Prêmio de participação")    
+    default:
+        console.log("Seu Ranking é abaixo do TOP 3")
+        break        
+}
