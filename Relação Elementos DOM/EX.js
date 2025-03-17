0ea6f4c8c1beaf933bd4273e2b34369c73a50262
Guilherme Dias Machado <pagani.dias2@gmail.com>
2025-03-17 13:59:19 -0300

    Relação elementos DOM p2

---
+++ Relação Elementos DOM/EX.js
@@ -0,0 +1,29 @@
+const caixa1=document.querySelector("#caixa1")
+const btn_c=[...document.querySelectorAll(".curso")]
+const c1_2=document.querySelector("#c1_2")
+
+console.log(c1_2.parentNode.parentNode.children[4])
+
+// console.log(caixa1.hasChildNodes())
+// console.log(btn_c[0].hasChildNodes())
+// console.log(btn_c[0].childNodes)
+
+// if(caixa1.children.length > 0){
+//     console.log("Possui filhos")
+// }else{
+//     console.log("Não possui filhos")
+// }
+
+// console.log(caixa1.children.length > 0 ? "Possui filhos": "Não possui filhos")
+
+// console.log(caixa1.children[1].innerHTML="Teste")
+//console.log(document.getRootNode())
+//console.log(btn_c[0].getRootNode())
+//console.log(btn_c[0].ownerDocument)
+
+//console.log(caixa1.children[caixa1.children.length-1])
+
+// console.log(caixa1.firstElementChild)
+// console.log(caixa1.lastElementChild)
+// console.log(caixa1.childElementCount)
+// console.log(caixa1.children)// 
