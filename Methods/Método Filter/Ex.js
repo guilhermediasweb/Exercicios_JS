57a93a2d85ac4db7cc889b5e7facfbbbe24734d3
Guilherme Dias Machado <pagani.dias2@gmail.com>
2025-03-19 14:59:26 -0300

    Filter P2

--- Methods/Método Filter/Ex.js
+++ Methods/Método Filter/Ex.js
@@ -1,23 +1,40 @@
-// const filterMaior18=(valor)=>{
-//     if(valor >= 18){
-//         return valor
-//     }
-// } Método 1
+const caixaCursos=document.querySelector("#caixaCursos")
+const btn_c=[...document.querySelectorAll(".curso")]
+const c1_2=document.querySelector("#c1_2")
+const cursos=["HTML","CSS","Javascript","PHP","React","MySQL","ReactNative"]
+const btnCursoSelecionado=document.getElementById("btnCursoSelecionado")
 
+cursos.map((el,chave)=>{
+    const novoElemento=document.createElement("div")
+    novoElemento.setAttribute("id","c"+chave)
+    novoElemento.setAttribute("class","curso c1")
+    novoElemento.innerHTML=el
 
-const idades=[15,21,30,17,44,12,50]
-// const maior=idades.filter(filterMaior18) Método 1
-const maior=idades.filter((valor)=>{
-    if(valor >= 18){
-        return valor
-    }
-}) // Método 2 com uso de arrow function
+    const comandos=document.createElement("div")
+    comandos.setAttribute("class","comandos")
 
-const menor=idades.filter((valor)=>{
-    if(valor <= 18){
-        return valor
-    }
+    const rb=document.createElement("input")
+    rb.setAttribute("type","radio")
+    rb.setAttribute("nome","curso")
+
+    comandos.appendChild(rb)
+
+    novoElemento.appendChild(comandos)
+
+    caixaCursos.appendChild(novoElemento)
 })
-console.log(idades) //idades
-console.log(maior) //impressão de valores maiores que 18
-console.log(menor) //Impressão dos valores menores que 18
+
+btnCursoSelecionado.addEventListener("click",(evt)=>{
+    const todosRadios=[...document.querySelectorAll("input[type=radio]")]
+    let radioSelecionado=todosRadios.filter((ele,ind,arr)=>{
+        return ele.checked
+    })
+    
+    radioSelecionado=radioSelecionado[0] // retorna somente o elemento selecionado.
+    const cursoSelecionado=radioSelecionado.parentNode.parentNode.firstChild.textContent // forma 1 de visualizar o parente e filho
+    //const cursoSelecionado=radioSelecionado.//parentNode.previousSibling.textContent // forma 2 de visualizar parente e filho
+    //console.log(todosRadios)
+    //console.log(radioSelecionado)
+    //console.log(cursoSelecionado) Visualização de parente e filho.
+    alert("Curso selecionado: " + cursoSelecionado) // selecionando o curso
+})
