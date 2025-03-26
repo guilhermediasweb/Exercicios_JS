e85d1f83212c32bd23f6da7e1d45274184b552c8
Guilherme Dias Machado <pagani.dias2@gmail.com>
2025-03-26 15:20:45 -0300

    Iteradores

---
+++ Funções/Iteradores/Aula_51.js
@@ -0,0 +1,15 @@
+//nst valores=[1,2,3,4]
+//nst it_valores=valores[Symbol.iterator]()
+
+const texto="Youtube"
+const it_texto=texto[Symbol.iterator]()
+
+console.log(texto)
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
