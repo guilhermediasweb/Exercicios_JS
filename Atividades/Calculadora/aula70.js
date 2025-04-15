39146c78a4ed79567df51a584288f48a70d1102d
Guilherme Dias Machado <pagani.dias2@gmail.com>
2025-04-15 13:33:43 -0300

    Calculadora p2

--- Atividades/Calculadora/aula70.js
+++ Atividades/Calculadora/aula70.js
@@ -4,14 +4,65 @@ const teclaRes=document.querySelector("#tigual")
 
 const display=document.querySelector(".display")
 
+const ton=document.getElementById("ton")
+const tlimpar=document.getElementById("tlimpar")
+const tigual=document.getElementById("tigual")
+
+let sinal=false
+let decimal=false
+
 teclasNum.forEach((el)=>{
     el.addEventListener("click",(evt)=>{
-        display.innerHTML+=evt.target.innerHTML
+        sinal=false
+        
+        if(evt.target.innerHTML==","){
+           if(!decimal){
+            decimal=true
+            if(display.innerHTML=="0"){
+                display.innerHTML="0,"
+            }else{
+                display.innerHTML+=evt.target.innerHTML
+            }
+            
+           }
+
+        }else{
+            if(display.innerHTML=="0"){
+                display.innerHTML=""
+            }
+            display.innerHTML+=evt.target.innerHTML
+        }
+        
     })
 })
 
 teclasop.forEach((el)=>{
     el.addEventListener("click",(evt)=>{
-        display.innerHTML+=evt.target.innerHTML
+        if(!sinal){
+            sinal=true
+            if(display.innerHTML=="0"){
+                display.innerHTML=""
+            }
+            if(evt.target.innerHTML=="x"){
+                display.innerHTML+="*"
+            }else{
+                display.innerHTML+=evt.target.innerHTML
+            }
+        }
+        
     })
 })
+
+tlimpar.addEventListener("click",(evt)=>{
+    sinal=false
+    decimal=false
+    display.innerHTML="0"
+})
+
+tigual.addEventListener("click",(evt)=>{
+    sinal=false
+    decimal=false
+    const res=eval(display.innerHTML)
+    display.innerHTML=res
+})
+
